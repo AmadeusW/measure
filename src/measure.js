01dcdbb364965cc6bc81b58221dfed0c55e90fbb
Amadeusz Wieczorek <git@amadeusw.com>
2017-09-14 08:21:26 -0700

    nice rendering

--- src/measure.js
+++ src/measure.js
@@ -5,12 +5,14 @@ var image = new Image();
 image.src = imageUrl;
 //document.getElementById("image").setAttribute("src", imageUrl);
 
-window.onload = function() {
+image.onload = function() {
     var c=document.getElementById("myCanvas");
     var ctx=c.getContext("2d");
     var img=document.getElementById("image");
-    ctx.drawImage(image, 0, 0, image.width, image.height,
-                         0, 0, c.width, c.height);
+    c.width = image.naturalWidth;
+    c.height = image.naturalHeight;
+    ctx.drawImage(image, 0, 0);//, image.width, image.height);//,
+                         //0, 0, c.width, c.height);
     c.addEventListener('click', onClicked, false);
 };
 
@@ -19,6 +21,7 @@ var fixedX = 0;
 
 function onClicked(e) {
   console.log("onClicked", e);
+  document.getElementById("debug").innerHTML = e.offsetY;
   if (!verticalSet) {
       verticalSet = true;
       fixedX = e.offsetX;
@@ -33,14 +36,16 @@ function onClicked(e) {
 function markLine(x) {
   var c=document.getElementById("myCanvas");
   var ctx=c.getContext("2d");
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = 'rgba(0,0,0,0.2)';
   ctx.beginPath();
   ctx.moveTo(x, 0);
-  ctx.lineTo(x, 200);
+  ctx.lineTo(x, image.naturalHeight);
   ctx.stroke();
 }
 
 function mark(x, y) {
     var c=document.getElementById("myCanvas");
     var ctx=c.getContext("2d");
-    ctx.fillRect(x, y, 2, 2);
+    ctx.fillRect(x-1, y, 2, 1);
   }
