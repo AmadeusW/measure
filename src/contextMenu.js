251d04481a993e3357c545d2694cfa1396bd2272
Amadeusz Wieczorek <git@amadeusw.com>
2017-09-15 00:02:21 -0700

    moves extension files out of src directory

--- src/contextMenu.js
+++ src/contextMenu.js
@@ -8,7 +8,7 @@ function onContextMenuClick(e) {
     var b = chrome.extension.getBackgroundPage();
     var image = e.srcUrl;
     b.console.log(image);
-    chrome.tabs.create({url: chrome.extension.getURL('measure.html?image='+image)}, function(tab) {
+    chrome.tabs.create({url: chrome.extension.getURL('src/measure.html?image='+image)}, function(tab) {
         chrome.tabs.executeScript(tab.id, {code:"console.log(image)"});
         b.console.log(tab);
         //tab.console.log(image);
